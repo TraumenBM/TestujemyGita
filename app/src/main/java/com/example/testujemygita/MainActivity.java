17eea388b61899136286f2ecb1b402fe2aa45033
Dominik Wąs <Traumen@o2.pl>
2022-11-07 07:44:08 +0100

    CW_6 - Data i godzina

--- app/src/main/java/com/example/testujemygita/MainActivity.java
+++ app/src/main/java/com/example/testujemygita/MainActivity.java
@@ -1,10 +1,27 @@
 package com.example.testujemygita;
-
+import android.view.View;
+import android.widget.TextView;
+import java.util.Calendar;
+import java.util.TimeZone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void ustaw_date(View v){
+        int rok, miesiac, dzien, godzina, minuta, sekunda;
+        Calendar calendar=Calendar.getInstance();
+        rok=calendar.get(Calendar.YEAR);
+        miesiac=calendar.get(Calendar.MONTH);
+        dzien=calendar.get(Calendar.DAY_OF_MONTH);
+        godzina=calendar.get(Calendar.HOUR_OF_DAY);
+        minuta=calendar.get(Calendar.MINUTE);
+        sekunda=calendar.get(Calendar.SECOND);
+
+        TextView panel=(TextView) findViewById(R.id.tekst_1);
+        panel.setText(rok+"-"+miesiac+"-"+dzien+"-"+godzina+"-"+minuta+"-"+sekunda);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
